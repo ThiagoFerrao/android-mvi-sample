f24b2e8f7e2a5c30293adf879cc91aafa075a1cf
Thiago Cruz <thiago.ferrao91@gmail.com>
2019-10-29 01:37:34 -0300

    adds testing dependencies

--- app/src/main/java/app/application/BaseApplication.kt
+++ app/src/main/java/app/application/BaseApplication.kt
@@ -3,12 +3,15 @@ package app.application
 import android.app.Application
 import home.di.homeModule
 import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
 
 class BaseApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         startKoin {
+            androidLogger(Level.DEBUG)
             androidContext(this@BaseApplication)
             modules(homeModule)
         }
